a966b3551014c3a37b1f6f6ccfa8998ae7d5b3cb
Rin.M <quino@RM.lan>
2022-05-26 04:45:07 +0300

    password to hash

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,9 +8,16 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractCrudController
 {
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -30,5 +37,28 @@ class UserController extends AbstractCrudController
         yield TextField::new('password')
             ->hideOnIndex();
         yield ArrayField::new('roles');
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->encodePassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->encodePassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function encodePassword(User $user)
+    {
+        if ($user->getPassword() !== null) {
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $user->getPassword(),
+            );
+            $user->setPassword($hashedPassword);
+        }
     }
 }
